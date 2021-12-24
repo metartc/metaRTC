4c5f35c4799d568ebd2b2ebfcc2c3187cb0be8b4
yangrtc <yangrtc@aliyun.com>
2021-12-24 20:11:24 +0800

    2.0

--- include/yangutil/sys/YangSocket.h
+++
@@ -1,8 +0,0 @@
-﻿
-#ifndef YANGSOCKETUTIL_H_
-#define YANGSOCKETUTIL_H_
-#include <string>
-
-std::string yang_getLocalInfo(void);
-void yang_getIp( std::string domain, std::string& ip);
-#endif /* YANGSOCKETUTIL_H_ */
