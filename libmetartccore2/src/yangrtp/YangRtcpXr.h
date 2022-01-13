0e0de3d64a5c96fee33b3811b0b87ee4cb9bf874
yangrtc <yangrtc@aliyun.com>
2022-01-13 19:37:54 +0800

    2.0.094

---
+++ libmetartccore2/src/yangrtp/YangRtcpXr.h
@@ -0,0 +1,12 @@
+
+#ifndef SRC_YANGRTP_YANGRTCPXR_H_
+#define SRC_YANGRTP_YANGRTCPXR_H_
+
+#ifdef __cplusplus
+extern "C"{
+#include <yangrtp/YangCRtcpXr.h>
+}
+#else
+#include <yangrtp/YangCRtcpXr.h>
+#endif
+#endif /* SRC_YANGRTP_YANGRTCPXR_H_ */
---
+++ libmetartccore2/src/yangrtp/YangRtpRecvNack.h
@@ -0,0 +1,12 @@
+#ifndef SRC_YANGRTP_YANGRTPRECVNACK_H_
+#define SRC_YANGRTP_YANGRTPRECVNACK_H_
+
+#ifdef __cplusplus
+extern "C"{
+#include <yangrtp/YangCRtpRecvNack.h>
+}
+#else
+#include <yangrtp/YangCRtpRecvNack.h>
+#endif
+
+#endif /* SRC_YANGRTP_YANGRTPRECVNACK_H_ */
