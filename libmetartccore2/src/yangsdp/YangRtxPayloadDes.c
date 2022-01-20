1741bf3d45f328284513ee5d0eafe26e13238b49
yangrtc <yangrtc@aliyun.com>
2022-01-20 09:09:51 +0800

    2.0

--- libmetartccore2/src/yangsdp/YangRtxPayloadDes.c
+++
@@ -1,9 +0,0 @@
-
-#include "../../../libmetartccore2/src/yangsdp/YangRtxPayloadDes.h"
-
-#include <string.h>
-void yang_init_rtxPayloaddes(YangRtxPayloadDes* des,uint8_t pt, char* encode_name, int32_t sample){
-	des->sample=sample;
-	des->pt=pt;
-	strcpy(des->name,(char*)"rtx");
-}
