a1462322e60c6e1a8fd6201939f053f483797925
yangrtc <yangrtc@aliyun.com>
2022-01-25 17:32:42 +0800

    2.0

--- libmetartccore2/src/yangcsrs/YangSrsConnection.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef YANGRTP_YangSrsConnection_H_
-#define YANGRTP_YangSrsConnection_H_
-
-#include <yangwebrtc/YangRtcSessionH.h>
-int32_t yang_srs_connectRtcServer(YangRtcSession* session);
-
-#endif
