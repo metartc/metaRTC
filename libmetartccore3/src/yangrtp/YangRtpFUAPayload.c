ea8be4ac22d6678b329f082da6262cb3eb69d71c
yangrtc <yangrtc@aliyun.com>
2022-03-07 16:19:45 +0800

    3.0

--- libmetartccore3/src/yangrtp/YangRtpFUAPayload.c
+++
@@ -1,7 +0,0 @@
-//
-// Copyright (c) 2019-2022 yanggaofeng
-//
-#include <yangrtp/YangRtpConstant.h>
-#include <yangrtp/YangRtpFUAPayload.h>
-#include <yangutil/sys/YangLog.h>
-
--- metaplayer3/yangplayer_config.h
+++
@@ -1,8 +0,0 @@
-﻿//
-// Copyright (c) 2019-2022 yanggaofeng
-//
-#ifndef YANGPLAYER_COMMON_H
-#define YANGPLAYER_COMMON_H
-#define Yang_Listen_MouseAndKeyboard 0
-
-#endif // YANGPLAYER_COMMON_H
