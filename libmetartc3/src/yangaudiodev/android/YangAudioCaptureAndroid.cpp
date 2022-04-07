65defca357b81856b5cc1d1d9e60d8632983682c
yangrtc <yangrtc@aliyun.com>
2022-04-07 16:33:30 +0800

    3.0.058

---
+++ libmetartc3/src/yangaudiodev/android/YangAudioCaptureAndroid.cpp
@@ -0,0 +1,83 @@
+﻿
+//
+// Copyright (c) 2019-2022 yanggaofeng
+//
+#ifdef __ANDROID__
+#include <yangaudiodev/android/YangAudioCaptureAndroid.h>
+#include <yangavutil/audio/YangMakeWave.h>
+#include <yangavutil/audio/YangAudioUtil.h>
+#include "stdlib.h"
+#include <stdio.h>
+#include "memory.h"
+#include "malloc.h"
+
+YangAudioCaptureAndroid::YangAudioCaptureAndroid(YangContext *pcontext) //:YangAudioCapture(pcontext)
+		{
+
+	m_context = pcontext;
+    m_ahandle = new YangAudioCaptureHandle(pcontext);
+	aIndex = 0;
+	m_size = 0;
+	m_loops = 0;
+	m_channel = pcontext->avinfo.audio.channel;
+	m_sample = pcontext->avinfo.audio.sample;
+
+	m_audioAndroid=(YangAudioAndroid*)calloc(sizeof(YangAudioAndroid),1);
+
+}
+
+YangAudioCaptureAndroid::~YangAudioCaptureAndroid() {
+	/*if (m_loops) {
+		stop();
+		while (m_isStart) {
+			yang_usleep(500);
+		}
+	}*/
+	closeAudio();
+}
+void YangAudioCaptureAndroid::closeAudio() {
+
+	yang_destroy_audioAndroid(m_audioAndroid);
+	yang_free(m_audioAndroid);
+
+}
+void YangAudioCaptureAndroid::setCatureStart() {
+	m_ahandle->isBuf = 1;
+}
+void YangAudioCaptureAndroid::setCatureStop() {
+	m_ahandle->isBuf = 0;
+}
+void YangAudioCaptureAndroid::setOutAudioBuffer(YangAudioBuffer *pbuffer) {
+	m_ahandle->setOutAudioBuffer(pbuffer);
+}
+void YangAudioCaptureAndroid::setPlayAudoBuffer(YangAudioBuffer *pbuffer) {
+	m_ahandle->m_aecPlayBuffer = pbuffer;
+}
+void YangAudioCaptureAndroid::setAec(YangRtcAec *paec) {
+	m_ahandle->m_aec = paec;
+}
+void YangAudioCaptureAndroid::setInAudioBuffer(vector<YangAudioPlayBuffer*> *pal) {
+
+}
+void YangAudioCaptureAndroid::setPreProcess(YangPreProcess *pp) {
+
+}
+
+
+int32_t YangAudioCaptureAndroid::init() {
+	yang_create_audioAndroid_record(m_audioAndroid,m_ahandle, m_sample,m_channel);
+	return Yang_Ok;
+}
+
+void YangAudioCaptureAndroid::startLoop() {
+	// loops = 5000000 / val;
+	m_loops = 1;
+
+	m_loops = 0;
+
+}
+
+void YangAudioCaptureAndroid::stopLoop() {
+	m_loops = 0;
+}
+#endif
---
+++ libmetartc3/src/yangpush/YangSendVideoImpl.cpp
@@ -0,0 +1,44 @@
+//
+// Copyright (c) 2019-2022 yanggaofeng
+//
+#include <yangpush/YangSendVideoImpl.h>
+
+YangSendVideoImpl::YangSendVideoImpl() {
+	m_outVideoBuffer=NULL;
+	m_outPreVideoBuffer=NULL;
+	m_width=1920;
+	m_height=1080;
+	m_buf=NULL;
+	m_len=0;
+	memset(&m_videoFrame,0,sizeof(YangFrame));
+
+}
+
+YangSendVideoImpl::~YangSendVideoImpl() {
+	yang_deleteA(m_buf);
+}
+void YangSendVideoImpl::init(int32_t wid,int32_t hei){
+	m_width=wid;
+	m_height=hei;
+}
+void YangSendVideoImpl::putVideoRgba(uint8_t *data, int len,int64_t timestamp) {
+	if(m_buf==NULL){
+		m_len=m_width*m_height*3/2;
+		m_buf=new uint8_t[m_len];
+
+	}
+
+	m_yuv.bgratoI420(data,m_buf,m_width,m_height);
+	putVideoI420(m_buf,m_len,timestamp);
+
+}
+
+void YangSendVideoImpl::putVideoI420(uint8_t *data, int len,int64_t timestamp) {
+	m_videoFrame.payload=data;
+	m_videoFrame.nb=len;
+	m_videoFrame.pts=timestamp;
+	if(m_outVideoBuffer) m_outVideoBuffer->putVideo(&m_videoFrame);
+	if(m_outPreVideoBuffer) m_outPreVideoBuffer->putVideo(&m_videoFrame);
+
+
+}
