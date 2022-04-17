afd78e0b033d6c466b2f8573e6756b70d7de4db3
yangrtc <yangrtc@aliyun.com>
2022-04-17 16:30:30 +0800

    3.0.063

---
+++ libmetartccore3/src/ffmpeg/yang_h264_initExtra.h
@@ -0,0 +1,13 @@
+//
+// Copyright (c) 2019-2022 yanggaofeng
+//
+
+#ifndef LIBAVFORMAT_YANG_H264_INITEXTRA_H_
+#define LIBAVFORMAT_YANG_H264_INITEXTRA_H_
+
+#include "libavformat/avformat.h"
+ 
+int av_yang_h264_initExtra( AVCodecContext *avctx,AVPacket *avpkt);
+
+
+#endif /* LIBAVFORMAT_YANG_H264_INITEXTRA_H_ */
