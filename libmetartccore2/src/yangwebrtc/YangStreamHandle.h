3cd3e32f185822775ee37b29a04b2a26cb294fd2
yangrtc <yangrtc@aliyun.com>
2022-04-21 08:58:36 +0800

    2.0.127

---
+++ libmetartccore2/src/yangwebrtc/YangStreamHandle.h
@@ -0,0 +1,10 @@
+//
+// Copyright (c) 2019-2022 yanggaofeng
+//
+
+#ifndef SRC_YANGWEBRTC_YANGSTREAMHANDLE_H_
+#define SRC_YANGWEBRTC_YANGSTREAMHANDLE_H_
+
+#include <yangwebrtc/YangRtcContext.h>
+int32_t yang_stream_parseUrl(char* url,YangStreamConfig* stream,YangAVContext* context,YangStreamOptType opt);
+#endif /* SRC_YANGWEBRTC_YANGSTREAMHANDLE_H_ */
