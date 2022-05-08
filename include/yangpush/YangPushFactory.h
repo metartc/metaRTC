c10a152d01e4c99459b6ab142b0685177074acc8
yangrtc <yangrtc@aliyun.com>
2022-05-08 15:57:26 +0800

    4.0.008

---
+++ include/yangpush/YangPushFactory.h
@@ -0,0 +1,19 @@
+﻿//
+// Copyright (c) 2019-2022 yanggaofeng
+//
+#ifndef INCLUDE_YANGPUSH_YANGPUSHFACTORY_H_
+#define INCLUDE_YANGPUSH_YANGPUSHFACTORY_H_
+#include <yangpush/YangPushHandle.h>
+#include <yangutil/sys/YangSysMessageHandle.h>
+#include <yangpush/YangSendVideoI.h>
+class YangPushFactory {
+public:
+	YangPushFactory();
+	virtual ~YangPushFactory();
+	YangPushHandle* createPushHandle(bool hasAudio,bool initVideo,int pvideotype,YangVideoInfo *screenvideo, YangVideoInfo *outvideo,YangContext* pcontext,YangSysMessageI* pmessage);
+        YangSysMessageHandle* createPushMessageHandle(bool hasAudio,bool initVideo,int pvideotype,YangVideoInfo *screenvideo, YangVideoInfo *outvideo,YangContext* pcontext,YangSysMessageI* pmessage,YangSysMessageHandleI* pmessagehandle);
+	YangVideoBuffer* getPreVideoBuffer(YangSysMessageHandle* pmessageHandle);
+	YangSendVideoI* getSendVideo(YangSysMessageHandle* pmessageHandle);
+};
+
+#endif /* INCLUDE_YANGPUSH_YANGPUSHFACTORY_H_ */
---
+++ metapushstream4/video/yangrecordthread.h
@@ -0,0 +1,45 @@
+﻿//
+// Copyright (c) 2019-2022 yanggaofeng
+//
+#ifndef YANGTHREAD_H
+#define YANGTHREAD_H
+#include <QThread>
+#include <QVector>
+
+#include "YangPlayWidget.h"
+//#include "../yangutil/yangmeetingtype.h"
+#include "yangrecliving/YangLivingType.h"
+#include "yangplayer/YangWinPlayFactroy.h"
+#include "yangutil/buffer/YangVideoBuffer.h"
+#include "video/yangrecordvideowin.h"
+
+class YangRecordThread : public QThread
+{
+public:
+    YangRecordThread();
+    virtual ~YangRecordThread();
+     int32_t m_isLoop;
+    void initPara(YangContext *par);
+    YangPlayWidget *m_video;
+    YangVideoBuffer *m_videoBuffer;
+    int32_t m_sid;
+    void stopAll();
+    int32_t showType;
+
+private:
+    void render();
+    void closeAll();
+
+    //void initPlaySdl(YangRecordThreadWin *pwin);
+    int32_t m_isStart;
+    YangColor m_bgColor;
+    YangColor m_textColor;
+    int32_t m_videoPlayNum;
+    YangContext *m_para;
+    YangFrame m_frame;
+
+private:
+    virtual void run();
+};
+
+#endif // YANGTHREAD_H
