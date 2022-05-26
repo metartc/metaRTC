037060bff8b1704041b0b0939713eecdcd22378b
yangrtc <yangrtc@aliyun.com>
2022-05-26 14:18:23 +0800

    4.0.028

--- include/yangutil/sys/YangCUrl.h
+++ include/yangutil/sys/YangCUrl.h
@@ -10,12 +10,12 @@
 #define Yang_Websocket_Wss 1
 #define Yang_Websocket_Http 2
 #define Yang_Websocket_Https 3
-typedef struct YangUrlData{
+typedef struct {
 	int32_t netType;
 	int32_t port;
 	char server[30];
 	char app[20];
-	char stream[20];
+	char stream[128];
 }YangUrlData;
 //webrtc://host[:port]/app/stream
 int32_t yang_srs_url_parse(char* purl, YangUrlData* data);
