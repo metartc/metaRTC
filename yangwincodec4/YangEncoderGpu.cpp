ecdb13974e1f2df30d7ff365d3f2e6a5a225aa8a
yangrtc <yangrtc@aliyun.com>
2022-06-15 18:07:44 +0800

    4.0.051

---
+++ yangwincodec4/YangEncoderGpu.cpp
@@ -0,0 +1,63 @@
+﻿//
+// Copyright (c) 2019-2022 yanggaofeng
+//
+#include "YangEncoderGpu.h"
+#include <yangutil/sys/YangLog.h>
+YangEncoderGpu::YangEncoderGpu() {
+
+    m_encoder=NULL;
+    m_sendKeyframe=0;
+    yang_trace("\nGpu encoder is created!");
+}
+
+YangEncoderGpu::~YangEncoderGpu() {
+    yang_delete(m_encoder);
+}
+
+void YangEncoderGpu::sendMsgToEncoder(YangRtcEncoderMessage *msg){
+    if(msg->request==Yang_Req_Sendkeyframe){
+        m_sendKeyframe=1;
+    }else if(msg->request==Yang_Req_HighLostPacketRate){
+
+    }else if(msg->request==Yang_Req_LowLostPacketRate){
+
+    }
+}
+
+void YangEncoderGpu::setVideoMetaData(YangVideoMeta *pvmd) {
+
+}
+
+/*void YangEncoderGpu::initX264Param(YangVideoInfo *pvp,		YangVideoEncInfo *penc) {
+
+}*/
+bool YangEncoderGpu::isSupported()
+{
+    return false;
+}
+int32_t YangEncoderGpu::init(YangContext* pcontext,YangVideoInfo *pvp) {
+	if (m_isInit == 1)		return Yang_Ok;
+    if(m_encoder==NULL) m_encoder=new YangH264EncoderGpu(pvp,&pcontext->avinfo.enc);
+    if(m_encoder) m_encoder->init();
+
+	m_isInit = 1;
+
+	return Yang_Ok;
+
+}
+
+int32_t YangEncoderGpu::encode(YangFrame* pframe, YangEncoderCallback* pcallback) {
+    if(m_encoder&&m_encoder->encode(pframe)>0){
+        //  yang_trace("\n%d:",pframe->nb);
+         //   for(int j=0;j<50;j++) yang_trace("%02x,",pframe->payload[j]);
+        if(pcallback){
+            if(pframe->nb>YANG_VIDEO_ENCODE_BUFFER_LEN)
+                yang_error("frame size is %d,ignore,drop,please increase framebuffer",pframe->nb);
+            else
+                pcallback->onVideoData(pframe);
+        }
+
+        return Yang_Ok;
+    }
+	return 1;
+}
---
+++ yangwincodec4/codec/QsvCodec/QsvEncoder.h
@@ -0,0 +1,81 @@
+﻿#ifndef QSV_ENCODER_H
+#define QSV_ENCODER_H
+
+#include "mfxvideo++.h"
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <memory>
+
+struct QsvParams
+{
+	std::string codec;
+	uint32_t width;
+	uint32_t height;
+	uint32_t bitrate_kbps;
+	uint32_t framerate;
+	uint32_t gop;
+};
+
+class QsvEncoder
+{
+public:
+	QsvEncoder & operator=(const QsvEncoder &) = delete;
+	QsvEncoder(const QsvEncoder &) = delete;
+	QsvEncoder();
+	virtual ~QsvEncoder();
+
+	static bool IsSupported();
+
+	virtual bool Init(QsvParams& qsv_params);
+	virtual void Destroy();
+	
+	virtual bool IsInitialized() const 
+	{ return is_initialized_; }
+
+    virtual int Encode(const uint8_t* nv12_image, uint32_t width, uint32_t height,
+		uint8_t* out_buf, uint32_t out_buf_size);
+
+	virtual void ForceIDR();
+	virtual void SetBitrate(uint32_t bitrate_kbps);
+
+	virtual int GetSequenceParams(uint8_t* buffer, int buffer_size);
+
+private:
+	bool InitParams(QsvParams& qsv_params);
+	bool AllocateSurfaces();
+	void FreeSurface();
+	bool AllocateBuffer();
+	void FreeBuffer();
+	bool GetVideoParam();
+	int  EncodeFrame(int index, uint8_t* out_buf, uint32_t out_buf_size);
+
+	bool is_initialized_ = false;
+	bool use_d3d11_ = false;
+	bool use_d3d9_ = false;
+
+	mfxIMPL                mfx_impl_;
+	mfxVersion             mfx_ver_;
+	MFXVideoSession        mfx_session_;
+	mfxFrameAllocator      mfx_allocator_;
+	mfxVideoParam          mfx_enc_params_;
+	mfxVideoParam          mfx_video_params_;	
+	mfxFrameAllocResponse  mfx_alloc_response_;
+	mfxExtCodingOption     extended_coding_options_;
+	mfxExtCodingOption2    extended_coding_options2_;
+	mfxExtBuffer*          extended_buffers_[2];
+	mfxEncodeCtrl          enc_ctrl_;
+
+	std::unique_ptr<MFXVideoENCODE> mfx_encoder_;
+
+	mfxBitstream           mfx_enc_bs_;
+	std::vector<mfxU8>     bst_enc_data_;
+	std::vector<mfxFrameSurface1> mfx_surfaces_;
+
+	std::unique_ptr<mfxU8> sps_buffer_;
+	std::unique_ptr<mfxU8> pps_buffer_;
+	mfxU16 sps_size_ = 0;
+	mfxU16 pps_size_ = 0;
+};
+
+#endif
