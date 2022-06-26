7e080f7692f5b071053e064d2e6e34f80d63a829
yangrtc <yangrtc@aliyun.com>
2022-06-26 17:19:14 +0800

    5.0

--- libmetartccore5/src/yangp2p/YangP2pConnection.h
+++
@@ -1,9 +0,0 @@
-﻿//
-// Copyright (c) 2019-2022 yanggaofeng
-//
-#ifndef YANGRTP_YangP2pConnection_H_
-#define YANGRTP_YangP2pConnection_H_
-
-#include <yangrtc/YangRtcConnection.h>
-int32_t yang_p2p_connectRtcServer(char* ip,int32_t remotePort,char* localSdp,char* remoteSdp);
-#endif
