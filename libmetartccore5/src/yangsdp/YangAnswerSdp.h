277da3d294a3544e82a8f516852ca1c419512482
yangrtc <yangrtc@aliyun.com>
2022-07-23 16:22:59 +0800

    5.0

--- libmetartccore5/src/yangsdp/YangAnswerSdp.h
+++
@@ -1,9 +0,0 @@
-﻿//
-// Copyright (c) 2019-2022 yanggaofeng
-//
-#ifndef SRC_YANGSDP_YANGANSWERSDP_H_
-#define SRC_YANGSDP_YANGANSWERSDP_H_
-#include <yangrtc/YangRtcSession.h>
-int32_t yang_sdp_getAnswerSdp(YangRtcSession* session,char* answer);
-
-#endif /* SRC_YANGSDP_YANGANSWERSDP_H_ */
