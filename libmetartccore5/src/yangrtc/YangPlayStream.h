2082d020580bedf399250a59acb2a75b1d154175
yangrtc <yangrtc@aliyun.com>
2022-07-25 08:34:26 +0800

    5.0.053

---
+++ libmetartccore5/src/yangrtc/YangPlayStream.h
@@ -0,0 +1,14 @@
+﻿//
+// Copyright (c) 2019-2022 yanggaofeng
+//
+
+#ifndef YANGWEBRTC_YANGRTCPLAYSTREAM_H_
+#define YANGWEBRTC_YANGRTCPLAYSTREAM_H_
+
+#include <yangrtc/YangRtcContext.h>
+#include <yangrtc/YangPlay.h>
+
+void yang_create_rtcplay(YangRtcContext *context, YangRtcPlay* play,	YangRtpBuffer *rtpBuffer);
+void yang_destroy_rtcplay(YangRtcPlay* play);
+
+#endif /* YANGWEBRTC_YANGRTCPLAYSTREAM_H_ */
