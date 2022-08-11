3e4b7c77c436ba28a541fc0148bb943cc0a9a027
yangrtc <yangrtc@aliyun.com>
2022-08-11 09:15:30 +0800

    5.0.073

---
+++ libmetartccore5/src/yangrtp/YangRtcpNack.h
@@ -0,0 +1,30 @@
+//
+// Copyright (c) 2019-2022 yanggaofeng
+//
+#ifndef SRC_YANGRTP_YANGRTCPNACK_H_
+#define SRC_YANGRTP_YANGRTCPNACK_H_
+
+#include <stdint.h>
+#include <yangrtp/YangRtcpCommon.h>
+#include <yangrtp/YangRtp.h>
+   typedef struct YangPidBlp {
+        uint16_t pid;
+        uint16_t blp;
+        int32_t in_use;
+    }YangPidBlp;
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+void yang_create_rtcpNack(YangRtcpCommon* comm,uint32_t pssrc);
+void yang_destroy_rtcpNack(YangRtcpCommon* comm);
+void yang_rtcpNack_addSn(YangRtcpNack* nack,uint16_t sn);
+
+int32_t yang_decode_rtcpNack(YangRtcpCommon* comm,YangBuffer *buffer);
+int32_t yang_encode_rtcpNack(YangRtcpCommon* comm,YangBuffer *buffer);
+uint64_t yang_rtcpNack_nb_bytes();
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* SRC_YANGRTP_YANGRTCPNACK_H_ */
