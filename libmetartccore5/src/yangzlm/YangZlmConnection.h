409b778c834579f0f9656c96df440dc3609c066b
yangrtc <yangrtc@aliyun.com>
2022-08-18 21:39:26 +0800

    5.0.083

---
+++ libmetartccore5/src/yangzlm/YangZlmConnection.h
@@ -0,0 +1,11 @@
+//
+// Copyright (c) 2019-2022 yanggaofeng
+//
+#ifndef YangZlmConnection_H_
+#define YangZlmConnection_H_
+
+#include <yangrtc/YangRtcConnection.h>
+
+int32_t yang_zlm_connectRtcServer(YangRtcConnection* conn);
+
+#endif
