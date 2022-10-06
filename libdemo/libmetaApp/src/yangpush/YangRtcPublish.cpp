2d0309b9b6459675d3fe1af4d59c0ae77e88523b
yangrtc <yangrtc@aliyun.com>
2022-10-06 09:40:30 +0800

    6.0.019

---
+++ libdemo/libmetaApp/src/yangpush/YangRtcPublish.cpp
@@ -0,0 +1,295 @@
+﻿//
+// Copyright (c) 2019-2022 yanggaofeng
+//
+#include <yangpush/YangRtcPublish.h>
+
+#include <yangstream/YangStreamCapture.h>
+#include <yangstream/YangStreamType.h>
+#include <yangavutil/video/YangNalu.h>
+#include <yangavutil/video/YangMeta.h>
+#include <yangavutil/video/YangVideoEncoderMeta.h>
+#include <yangutil/sys/YangLog.h>
+#include <yangutil/yang_unistd.h>
+#include <stdio.h>
+void g_pushstream_sendData(void* context,YangFrame* msgFrame){
+	YangRtcPublish* push=(YangRtcPublish*)context;
+	push->publishMsg(msgFrame);
+}
+
+void g_pushstream_receiveMsg(void* user,YangFrame *msgFrame){
+	if(user==NULL) return;
+	YangRtcPublish* rtcHandle=(YangRtcPublish*)user;
+	rtcHandle->receiveMsg(msgFrame);
+
+}
+
+YangRtcPublish::YangRtcPublish(YangContext *pcontext) {
+	m_context = pcontext;
+
+	m_in_videoBuffer = NULL;
+	m_in_audioBuffer = NULL;
+	m_isStart = 0;
+	m_isConvert = 0;
+	m_vmd = NULL;
+	m_audioEncoderType = m_context->avinfo.audio.audioEncoderType;
+	m_netState = 1;
+	m_isInit = 0;
+	isPublished = 0;
+
+	m_transType=Yang_Webrtc;
+	notifyState=0;
+
+	m_context->channeldataSend.context=this;
+    m_context->channeldataSend.sendData=g_pushstream_sendData;
+}
+
+YangRtcPublish::~YangRtcPublish() {
+	if (m_isConvert) {
+		stop();
+		while (m_isStart) {
+			yang_usleep(1000);
+		}
+	}
+	m_context = NULL;
+	m_in_videoBuffer = NULL;
+	m_in_audioBuffer = NULL;
+	m_vmd = NULL;
+}
+
+
+int32_t YangRtcPublish::connectServer(int32_t puid){
+	return m_pushs.back()->connectSfuServer();
+}
+
+
+
+int32_t YangRtcPublish::reconnectMediaServer() {
+	return m_pushs.back()->connectSfuServer();
+}
+
+int32_t YangRtcPublish::publishMsg(YangFrame* msgFrame){
+
+    if(m_pushs.size()>0) {
+    	msgFrame->uid=m_pushs.front()->streamConfig->uid;
+    	return m_pushs.front()->on_message(msgFrame);
+    }
+	return 1;
+}
+
+int32_t YangRtcPublish::receiveMsg(YangFrame* msgFrame){
+
+	if(m_context->channeldataRecv.receiveData)
+        m_context->channeldataRecv.receiveData(m_context->channeldataRecv.context,msgFrame);
+	return Yang_Ok;
+}
+
+int32_t YangRtcPublish::init(int32_t nettype, char* server, int32_t pport,
+		char* app,char* stream) {
+
+	int32_t ret = 0;
+	YangStreamConfig streamconfig;
+	memset(&streamconfig,0,sizeof(YangStreamConfig));
+	strcpy(streamconfig.app,app);
+	streamconfig.streamOptType=Yang_Stream_Publish;
+
+	strcpy(streamconfig.remoteIp,server);
+	streamconfig.remotePort=pport;
+
+	strcpy(streamconfig.stream,stream);
+	streamconfig.uid=0;
+
+	streamconfig.localPort=m_context->avinfo.sys.rtcLocalPort;
+
+	streamconfig.recvCallback.context=this;
+	streamconfig.recvCallback.receiveMsg=g_pushstream_receiveMsg;
+
+	memcpy(&streamconfig.rtcCallback,&m_context->rtcCallback,sizeof(YangRtcCallback));
+
+	YangPeerConnection2* sh=new YangPeerConnection2(&m_context->avinfo,&streamconfig);
+	sh->init();
+	m_pushs.push_back(sh);
+
+
+
+	if (sh->isConnected())		return Yang_Ok;
+
+	ret = m_pushs.back()->connectSfuServer();
+
+	if (ret)		return ret;
+
+	yang_reindex(m_in_audioBuffer);
+	yang_reindex(m_in_videoBuffer);
+	return Yang_Ok;
+
+}
+
+int32_t YangRtcPublish::connectMediaServer() {
+	if(m_pushs.size()>0) return Yang_Ok;
+
+	return Yang_Ok;
+}
+
+int32_t YangRtcPublish::disConnectMediaServer() {
+	if (m_pushs.size() > 0) {
+		yang_delete(m_pushs.back());
+		m_pushs.clear();
+	}
+	return Yang_Ok;
+}
+
+void YangRtcPublish::stop() {
+	m_isConvert = 0;
+}
+
+void YangRtcPublish::run() {
+	m_isStart = 1;
+	startLoop();
+	m_isStart = 0;
+}
+
+void YangRtcPublish::setInAudioList(YangAudioEncoderBuffer *pbuf) {
+	m_in_audioBuffer = pbuf;
+}
+void YangRtcPublish::setInVideoList(YangVideoEncoderBuffer *pbuf) {
+	m_in_videoBuffer = pbuf;
+}
+void YangRtcPublish::setInVideoMetaData(YangVideoMeta *pvmd) {
+	m_vmd = pvmd;
+}
+
+void YangRtcPublish::startLoop() {
+
+	isPublished = 0;
+	m_isConvert = 1;
+
+	YangStreamCapture data;
+	memset(&data,0,sizeof(YangStreamCapture));
+	yang_create_streamCapture(&data);
+	YangFrame audioFrame;
+	YangFrame videoFrame;
+	memset(&audioFrame,0,sizeof(YangFrame));
+	memset(&videoFrame,0,sizeof(YangFrame));
+	data.initAudio(data.context,m_context->avinfo.sys.transType,m_context->avinfo.audio.sample, m_context->avinfo.audio.channel,
+			(YangAudioCodec) m_context->avinfo.audio.audioEncoderType);
+	data.initVideo(data.context,m_context->avinfo.sys.transType);
+	YangVideoCodec videoType =	(YangVideoCodec) m_context->avinfo.video.videoEncoderType;
+
+	int32_t ret = Yang_Ok;
+	isPublished = 1;
+	notifyState=1;
+	YangVideoMeta* vmd=NULL;
+	if(m_context->avinfo.enc.createMeta==0){
+		vmd=(YangVideoMeta*)calloc(sizeof( YangVideoMeta),1);
+	}
+
+	YangH264NaluData nalu;
+	YangPeerConnection2* stream=NULL;
+	while (m_isConvert == 1) {
+
+
+		if ((m_in_videoBuffer && m_in_videoBuffer->size() == 0)
+				&& (m_in_audioBuffer && m_in_audioBuffer->size() == 0)) {
+			yang_usleep(2000);
+			continue;
+		}
+		if (m_pushs.size() == 0) {
+			yang_usleep(500);
+			continue;
+		}
+		stream=m_pushs.back();
+
+		if(stream->isConnected()){
+			if(notifyState&&m_transType<Yang_Webrtc){
+						if(m_context) m_context->streams.sendRequest(0,0,Yang_Req_Connected);
+						notifyState=0;
+					}
+
+		}else{
+			yang_usleep(500);
+			continue;
+		}
+
+		if (m_in_audioBuffer && m_in_audioBuffer->size() > 0) {
+
+			audioFrame.payload = m_in_audioBuffer->getAudioRef(&audioFrame);
+			data.setAudioData(data.context,&audioFrame);
+
+			//for (i = 0; i < m_pushs.size(); i++) {
+                ret = stream->on_audio(data.getAudioFrame(data.context));
+				if (ret&&!stream->isConnected()) {
+					stream->close();
+					//yang_post_message(YangM_Sys_PushMediaServerError,m_pushs.back()->m_uid,NULL);
+				}
+			//}
+		}
+
+		if (m_in_videoBuffer && m_in_videoBuffer->size() > 0) {
+
+			videoFrame.payload = m_in_videoBuffer->getEVideoRef(&videoFrame);
+
+			if (videoFrame.frametype == YANG_Frametype_I) {
+
+				if (m_vmd) {
+					data.setVideoMeta(data.context,m_vmd->livingMeta.buffer,
+							m_vmd->livingMeta.bufLen, videoType);
+					//if (ret)	continue;
+				} else {
+					if (!vmd->isInit) {
+						if (videoType == Yang_VED_264) {
+							yang_createH264Meta(vmd, &videoFrame);
+							yang_getConfig_Flv_H264(&vmd->mp4Meta,
+									vmd->livingMeta.buffer,
+									&vmd->livingMeta.bufLen);
+						} else if (videoType == Yang_VED_265) {
+							yang_createH265Meta(vmd, &videoFrame);
+							yang_getConfig_Flv_H265(&vmd->mp4Meta,
+									vmd->livingMeta.buffer,
+									&vmd->livingMeta.bufLen);
+						}
+					}
+
+					data.setVideoMeta(data.context,vmd->livingMeta.buffer,vmd->livingMeta.bufLen, videoType);
+
+
+				}
+				data.setVideoFrametype(data.context,YANG_Frametype_Spspps);
+				data.setMetaTimestamp(data.context,videoFrame.pts);
+                ret = stream->on_video(data.getVideoFrame(data.context));
+
+
+
+				if (!m_context->avinfo.enc.createMeta) {
+
+					memset(&nalu, 0, sizeof(YangH264NaluData));
+					if (videoType == Yang_VED_264)
+						yang_parseH264Nalu(&videoFrame, &nalu);
+					else
+						yang_parseH265Nalu(&videoFrame, &nalu);
+
+
+					if (nalu.keyframePos > -1) {
+						videoFrame.payload += nalu.keyframePos + 4;
+						videoFrame.nb -= (nalu.keyframePos + 4);
+
+					} else {
+						videoFrame.payload = NULL;
+						continue;
+					}
+				}
+
+			}
+
+			data.setVideoData(data.context,&videoFrame, videoType);
+            ret = stream->on_video(data.getVideoFrame(data.context));
+
+			if (ret && !stream->isConnected()) {
+				stream->close();
+
+			}
+
+		}			//end
+	}
+	isPublished = 0;
+	yang_destroy_streamCapture(&data);
+	yang_free(vmd);
+}
---
+++ libdemo/libmetaApp/src/yangrecord/YangRecord.cpp
@@ -0,0 +1,188 @@
+﻿//
+// Copyright (c) 2019-2022 yanggaofeng
+//
+
+#include <yangrecord/YangRecord.h>
+#include <yangencoder/YangAudioEncoderMeta.h>
+#include <yangutil/sys/YangEndian.h>
+#include <yangutil/yang_unistd.h>
+#include <time.h>
+#include <memory.h>
+
+YangRecord::YangRecord(YangAudioInfo *paudio,YangVideoInfo *pvideo,YangVideoEncInfo *penc) {
+	m_audio=paudio;
+	m_video=pvideo;
+	m_enc=penc;
+	videoDestLen = 0;
+	preVideotimestamp = 0;
+	minusStamp = 0;
+	oldalltick = 0, alltick = 0;
+	isMp4 = 1;
+	curVideoTimestamp = 0;
+	basestamp=0;
+	m_prePauseTime=0,m_afterPauseTime=0;
+	m_pauseTime=0;
+	m_fileId=0;
+	m_alltime=0;
+	m_alltime1=0;
+	m_fileTimeLen=3600;
+	m_isCreateNewFile=0;
+	memset(&m_mp4Para,0,sizeof(m_mp4Para));
+	mp4 = NULL;
+	flv = NULL;
+
+}
+
+YangRecord::~YangRecord(void) {
+	m_audio=NULL;
+	m_video=NULL;
+	m_enc=NULL;
+	yang_free(m_mp4Para.fileName);
+	m_mp4Para.vmd=NULL;
+	yang_delete(mp4);
+	yang_delete(flv);
+}
+
+
+void YangRecord::writeAudioData(YangFrame* audioFrame) {
+	if (audioFrame->nb < 0 || audioFrame->nb > 1000)	return;
+	if (isMp4){
+		mp4->WriteAudioPacket(audioFrame);
+	}
+	else
+		flv->WriteAudioPacket(audioFrame);
+}
+
+
+void YangRecord::writeVideoData(YangFrame* videoFrame) {
+	if(m_alltime>m_fileTimeLen){
+		m_isCreateNewFile=1;
+		m_alltime=0;
+		m_alltime1=0;
+	}
+	if(m_isCreateNewFile&&videoFrame->frametype){
+
+		createNewfile();
+
+		m_isCreateNewFile=0;
+		m_alltime=0;
+		m_alltime1=0;
+	}
+	if (basestamp == 0)		basestamp = videoFrame->pts;
+	if (isMp4) {
+		alltick = (videoFrame->pts - basestamp-m_pauseTime) * 9 / 100;
+
+		minusStamp = alltick - oldalltick;
+		m_alltime1+=minusStamp;
+		m_alltime=m_alltime1/90000;
+
+		yang_put_be32((char*)videoFrame->payload,videoFrame->nb);
+		videoFrame->nb+=4;
+		videoFrame->pts=minusStamp;
+		mp4->WriteVideoPacket(videoFrame);
+
+		oldalltick=alltick;
+	} else {
+		curVideoTimestamp = (videoFrame->pts - basestamp-m_pauseTime) / 1000;
+		m_alltime=curVideoTimestamp/1000;
+
+		videoFrame->pts=curVideoTimestamp;
+		flv->WriteVideoPacket(videoFrame);
+	}
+
+}
+
+
+void YangRecord::pauseRec(){
+	m_prePauseTime=yang_get_milli_tick();//m_time.getMilliTick();
+}
+	void YangRecord::resumeRec(){
+
+		m_afterPauseTime=yang_get_milli_tick();//m_time.getMilliTick();
+		m_pauseTime+=m_afterPauseTime-m_prePauseTime;
+
+	}
+	void YangRecord::initRecPara(){
+
+		preVideotimestamp = 0;
+		minusStamp = 0;
+		oldalltick = 0, alltick = 0;
+		curVideoTimestamp = 0;
+		basestamp=0;
+		m_prePauseTime=0,m_afterPauseTime=0;
+		m_pauseTime=0;
+		//if(m_in_audioBuffer) m_in_audioBuffer->resetIndex();
+		//if(m_in_videoBuffer) m_in_videoBuffer->resetIndex();
+	}
+	void YangRecord::createNewfile(){
+		closeRec();
+		char filename1[128];
+		memset(filename1,0,128);
+		memcpy(filename1,m_mp4Para.fileName,strlen(m_mp4Para.fileName)-4);
+		m_fileId++;
+		char filename[255];
+		memset(filename,0,255);
+		sprintf(filename,"%s_%d.%s",filename1,m_fileId,isMp4?"mp4":"flv");
+		createFile(filename);
+		initRecPara();
+
+	}
+	void YangRecord::createFile(char* filename){
+		if (isMp4) {
+				mp4 = new YangMp4File(filename, m_video);
+				mp4->init(m_mp4Para.vmd->mp4Meta.sps, m_mp4Para.vmd->mp4Meta.spsLen);
+				uint8_t pasc[1024];
+				int32_t pasclen=0;
+				YangAudioEncoderMeta yem;
+				yem.createMeta(pasc,&pasclen);
+				memset(m_mp4Para.asc,0,10);
+				memcpy(m_mp4Para.asc,pasc,pasclen);
+				m_mp4Para.ascLen=pasclen;
+
+				mp4->WriteAudioInfo(pasc,pasclen,NULL, 0);
+				mp4->WriteVideoInfo(m_mp4Para.vmd->mp4Meta.vps, m_mp4Para.vmd->mp4Meta.vpsLen,m_mp4Para.vmd->mp4Meta.sps, m_mp4Para.vmd->mp4Meta.spsLen,
+						m_mp4Para.vmd->mp4Meta.pps, m_mp4Para.vmd->mp4Meta.ppsLen);
+			} else {
+				flv = new YangFlvWriter(filename, m_audio,m_video);
+				flv->framerate = m_video->frame;
+				flv->i_bitrate = (double)m_video->rate;
+				flv->i_level_idc = 31;
+				flv->Start();
+
+				flv->WriteVideoInfo(m_mp4Para.vmd->livingMeta.buffer, m_mp4Para.vmd->livingMeta.bufLen);
+				//flv->WriteAudioHeadPacket();
+
+			}
+
+	}
+void YangRecord::initPara(YangVideoMeta *p_vmd,char *filename, int32_t p_isMp4) {
+
+	isMp4 = p_isMp4;
+	m_mp4Para.vmd=p_vmd;
+	int32_t flen=strlen(filename);
+	m_mp4Para.fileName=(char*)malloc(flen+1);
+	memset(m_mp4Para.fileName,0,flen+1);
+	memcpy(m_mp4Para.fileName,filename,flen);
+	createFile(filename);
+
+}
+void YangRecord::setFileTimeLen(int32_t ptlen_min){
+	m_fileTimeLen=ptlen_min*60;
+}
+void YangRecord::closeRec() {
+	if (mp4 != NULL) {
+		mp4->closeMp4();
+
+		yang_delete(mp4);
+	}
+	if (flv != NULL) {
+		flv->Close();
+
+		yang_delete(flv);
+	}
+
+}
+
+
+
+
