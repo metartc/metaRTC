821f12543b9e9c0a601559114e906846b390a3c8
yangrtc <yangrtc@aliyun.com>
2023-03-24 10:30:43 +0800

    6.0.175

--- cmake_lib_android.sh
+++
@@ -1,7 +0,0 @@
-cd libmetartccore6
-./cmake_android.sh
-cd ../libmetartc6
-./cmake_android.sh
-cd ../demo/libmetaApp
-./cmake_android.sh
-
--- cmake_lib_x64.sh
+++
@@ -1,9 +0,0 @@
-cd libmetartccore6
-./cmake_x64.sh
-cd ../libmetartc6
-./cmake_x64.sh
-cd ../demo/libmetaApp
-./cmake_x64.sh
-cd ../libmetaCApp
-./cmake_x64.sh
-
