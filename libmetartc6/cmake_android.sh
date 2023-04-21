d7ffcfd141e3b2c4d2d3d10c3c089383667a610b
LeiZhi.Mai <maiyethom@163.com>
2023-04-21 10:45:14 +0800

    核心库安卓编译脚本优化

--- libmetartc6/cmake_android.sh
+++ libmetartc6/cmake_android.sh
@@ -1,27 +1,44 @@
+#!/bin/bash
+
 NDK_ROOT=/home/yang/pro/ndk
+#NDK_ROOT=/home/mlz/Android/ndk/android-ndk-r21e
+
 API=29
-rm -rf build
-mkdir build
-cd build
-#arm64-v8a armeabi x86_64
-ARCH=arm64-v8a
-cmake -G"Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=$NDK_ROOT/build/cmake/android.toolchain.cmake -DANDROID_NDK=$NDK_ROOT -DCMAKE_SYSTEM_NAME=Android -DANDROID_PLATFORM=android-${API} -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=${ARCH} -DAndroid=ON -DANDROID_STL=c++_static -DCMAKE_CXX_STANDARD=11 -DANDROID_NATIVE_API_LEVEL=${API} ..
-make
-if [ ! -d "../../bin/lib_android/arm64-v8a" ] ; then
-mkdir -p ../../bin/lib_android/arm64-v8a
-fi
-cp ./libmetartc6.a ../../bin/lib_android/arm64-v8a/
 
-cd ..
-rm -rf build
-mkdir build
-cd build
-ARCH=x86_64
-cmake -G"Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=$NDK_ROOT/build/cmake/android.toolchain.cmake -DANDROID_NDK=$NDK_ROOT -DCMAKE_SYSTEM_NAME=Android -DANDROID_PLATFORM=android-${API} -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI=${ARCH} -DAndroid=ON -DANDROID_STL=c++_static -DCMAKE_CXX_STANDARD=11 -DANDROID_NATIVE_API_LEVEL=${API} ..
-make
-if [ ! -d "../../bin/lib_android/x86_64" ] ; then
-mkdir -p ../../bin/lib_android/x86_64
-fi
-cp ./libmetartc6.a ../../bin/lib_android/x86_64/
+build() {
+    rm -rf build
+    mkdir build
+    cd build
 
+    echo "NDK_ROOT: ${NDK_ROOT}"
+    echo "ARCH: ${1}"
 
+    cmake -G"Unix Makefiles" \
+    -DCMAKE_SYSTEM_NAME=Android \
+    -DCMAKE_TOOLCHAIN_FILE=$NDK_ROOT/build/cmake/android.toolchain.cmake \
+    -DANDROID_NDK=$NDK_ROOT \
+    -DANDROID_PLATFORM=android-${API} \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DANDROID_ABI=$1 \
+    -DANDROID_STL=c++_static \
+    -DCMAKE_CXX_STANDARD=11 \
+    -DANDROID_NATIVE_API_LEVEL=${API} \
+    ..
+
+    make
+
+    if [ ! -d "../../bin/lib_android/$1" ] ; then
+        mkdir -p ../../bin/lib_android/$1
+    fi
+        cp ./libmetartc6.a ../../bin/lib_android/$1/
+
+    cd ..
+}
+
+build x86
+
+build x86_64
+
+build armeabi-v7a
+
+build arm64-v8a
