00d92c89ea10a0a08edda61d09c8a82be063aef6
yangrtc <yangrtc@aliyun.com>
2023-10-15 10:04:07 +0800

    7.0.062

---
+++ demo/metaplayer7_lvgl/yangplayer/YangPlayFactory.cpp
@@ -0,0 +1,30 @@
+﻿//
+// Copyright (c) 2019-2022 yanggaofeng
+//
+
+
+#include <yangplayer/YangPlayFactory.h>
+
+
+YangPlayFactory::YangPlayFactory() {
+
+
+}
+
+YangPlayFactory::~YangPlayFactory() {
+
+}
+
+YangAudioPlay* YangPlayFactory::createAudioPlay(YangAudioInfo *pcontext){
+#if !Yang_OS_WIN
+	return NULL;//if(pcontext->audioPlayType==1) return new YangAudioPlayAlsa(pcontext);
+#endif
+	return NULL;// new YangAudioPlaySdl(pcontext);
+}
+
+YangAudioPlay *YangPlayFactory::createAudioPlay(YangAudioPlayType paet,YangAudioInfo *pcontext){
+#if !Yang_OS_WIN
+	return NULL;//if(paet==Yang_AP_ALSA) return new YangAudioPlayAlsa(pcontext);
+#endif
+	return NULL;//new YangAudioPlaySdl(pcontext);
+}
