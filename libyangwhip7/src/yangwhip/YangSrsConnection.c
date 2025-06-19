609429c5fc149fc50a51d7909e4771c915b4b045
yangrtc <yangrtc@aliyun.com>
2025-06-19 14:11:01 +0800

    7.0.170

--- libyangwhip7/src/yangwhip/YangSrsConnection.c
+++ libyangwhip7/src/yangwhip/YangSrsConnection.c
@@ -27,23 +27,24 @@ static void yang_destroy_srsresponse(SrsSdpResponseType* srs){
 	yang_free(srs->sessionid);
 }
 
-static int32_t yang_sdp_querySrs(YangMetaConnection *conn,YangPeer* peer,SrsSdpResponseType* srs,int32_t isplay,char* ip,int32_t port,char* purl, char* psdp)
+static int32_t yang_sdp_querySrs(YangMetaConnection *conn,YangPeer* peer,SrsSdpResponseType* srs,int32_t isplay,yangbool isHttps,char* ip,int32_t port,char* purl, char* psdp)
 {
 
 	int32_t err=Yang_Ok;
-
+	char *p;
 	char* sdp=(char*)yang_calloc(1,Yang_SDP_BUFFERLEN);
 
+	YangJson *jcode,*server,*sessionid,*jsdp;
+	YangJsonReader reader;
 
-	if(yang_http_post(yangfalse,peer->peerInfo.familyType,sdp,ip, port, purl, (uint8_t*)psdp, yang_strlen(psdp))){
+	if(yang_https_post(yangfalse,peer->peerInfo.familyType,sdp,ip, port, purl, (uint8_t*)psdp, yang_strlen(psdp))){
 		yang_free(sdp);
 		return yang_error_wrap(1,"query srs sdp failure!");
 	}
 
-
-	YangJsonReader reader;
-	char* p=yang_strstr(sdp,"{");
-	if(p==NULL) return ERROR_STRING;
+	p=yang_strstr(sdp,"{");
+	if(p==NULL)
+		return ERROR_STRING;
 
 	if(yang_create_jsonReader(&reader,p)!=Yang_Ok){
 		yang_error("read srs response json error!");
@@ -51,7 +52,7 @@ static int32_t yang_sdp_querySrs(YangMetaConnection *conn,YangPeer* peer,SrsSdpR
 		goto cleanup;
 	}
 
-	YangJson* jcode = reader.getObjectItemCaseSensitive(reader.session, "code");
+	jcode = reader.getObjectItemCaseSensitive(reader.session, "code");
 	if (reader.isNumber(jcode))
 	{
 		err=jcode->valueint==0?Yang_Ok:ERROR_SERVER_ConnectFailure;
@@ -59,9 +60,11 @@ static int32_t yang_sdp_querySrs(YangMetaConnection *conn,YangPeer* peer,SrsSdpR
 	}
 
 	srs->retcode=jcode->valueint;
-	YangJson* server = reader.getObjectItemCaseSensitive(reader.session, "server");
-	YangJson* sessionid = reader.getObjectItemCaseSensitive(reader.session, "sessionid");
-	YangJson* jsdp = reader.getObjectItemCaseSensitive(reader.session, "sdp");
+
+	server = reader.getObjectItemCaseSensitive(reader.session, "server");
+	sessionid = reader.getObjectItemCaseSensitive(reader.session, "sessionid");
+	jsdp = reader.getObjectItemCaseSensitive(reader.session, "sdp");
+
 	if (reader.isString(server) && (server->valuestring != NULL))
 	{
 		srs->serverIp=(char*)yang_calloc(yang_strlen(server->valuestring)+1,1);
@@ -86,21 +89,22 @@ static int32_t yang_sdp_querySrs(YangMetaConnection *conn,YangPeer* peer,SrsSdpR
 	yang_destroy_jsonReader(&reader);
 	yang_free(sdp);
 	return err;
-
-
-
 }
 
-static int32_t yang_srs_getSignal(YangMetaConnection *conn,YangPeer* peer,char* url,SrsSdpResponseType* srs,char* sdp) {
+static int32_t yang_srs_getSignal(YangMetaConnection *conn,YangPeer* peer,char* url,SrsSdpResponseType* srs,char* sdp,yangbool isHttps) {
 	int32_t err = Yang_Ok;
 	YangRtcDirection role=peer->peerInfo.direction;
+	char *srsSdp;
 	const char* roleStr=role==YangRecvonly?"play":"publish";
 	char apiStr[256]={0};
 	char streamurl[1024]={0};
 	char apiurl[512] ;
 	YangUrlData urlData;
-	memset(urlData.server,0,sizeof(urlData.server));
+	YangJsonWriter writer;
+
+	yang_memset(urlData.server,0,sizeof(urlData.server));
 	urlData.port=0;
+
 	if(yang_url_parse(peer->peerInfo.familyType,url,&urlData))
 		return 1;
 
@@ -108,19 +112,19 @@ static int32_t yang_srs_getSignal(YangMetaConnection *conn,YangPeer* peer,char* 
 	yang_sprintf(streamurl,"webrtc://%s:%d/%s/%s",urlData.server,
 			urlData.port, urlData.app, urlData.stream);
 
-	YangJsonWriter writer;
+
 	yang_create_jsonWriter(&writer);
 
 	writer.addStringToObject(writer.session,"capi",apiStr);
 	writer.addStringToObject(writer.session,"streamurl",streamurl);
 	writer.addNullToObject(writer.session,"clientip");
 	writer.addStringToObject(writer.session,"sdp",sdp);
-	char *srsSdp=writer.printUnformatted(writer.session);
+	srsSdp=writer.printUnformatted(writer.session);
 
 	yang_memset(apiurl,0,sizeof(apiurl));
 
 	yang_sprintf(apiurl, "rtc/v1/%s/", roleStr);
-	err=yang_sdp_querySrs(conn,peer,srs,role==YangRecvonly?1:0,urlData.server,urlData.port,apiurl, srsSdp);
+	err=yang_sdp_querySrs(conn,peer,srs,role==YangRecvonly?1:0,isHttps,urlData.server,urlData.port,apiurl, srsSdp);
 
 
 	yang_destroy_jsonWriter(&writer);
@@ -129,19 +133,19 @@ static int32_t yang_srs_getSignal(YangMetaConnection *conn,YangPeer* peer,char* 
 	return err;
 }
 
-int32_t yang_srs_connectRtcServer(YangMetaConnection *conn,YangPeer* peer,char* url){
+int32_t yang_srs_connectRtcServer(YangMetaConnection *conn,YangPeer* peer,char* url,yangbool isHttps){
 
-	int err=Yang_Ok;
+	int32_t err=Yang_Ok;
+	char *tsdp=NULL;
 	SrsSdpResponseType srs;
+
 	yang_memset(&srs,0,sizeof(SrsSdpResponseType));
 
-
-	char *tsdp=NULL;
 	peer->peerInfo.iceMode = YangIceModeLite;
 	conn->createOffer(peer, &tsdp);
 	conn->setLocalDescription(peer,tsdp);
 
-	if ((err=yang_srs_getSignal(conn,peer,url,&srs,tsdp))  == Yang_Ok) {
+	if ((err=yang_srs_getSignal(conn,peer,url,&srs,tsdp,isHttps))  == Yang_Ok) {
 		conn->setRemoteDescription(peer,srs.sdp);
 
 	}
--- libyangwhip7/src/yangwhip/YangWhip.c
+++ libyangwhip7/src/yangwhip/YangWhip.c
@@ -11,7 +11,6 @@
 #include <yangutil/sys/YangCUrl.h>
 #include <yangutil/sys/YangCString.h>
 #include <yangutil/sys/YangHttp.h>
-
 #define Yang_SDP_BUFFERLEN 1024*12
 
 typedef struct{
@@ -22,36 +21,51 @@ typedef struct{
 
 
 static int32_t yang_whip_getSignal(YangMetaConnection *conn,YangPeer* peer,char* url,char** premoteSdp,char* localSdp) {
-	int32_t err=Yang_Ok;
+	int32_t err=Yang_Ok,len;
 	YangUrlData urlData={0};
+
+	yangbool isHttps=yang_memcmp(url,"https:",6)==0?yangtrue:yangfalse;
+	char *remoteSdp,*p,*p2,*endp,*sdp;
+
 	yang_http_url_parse(peer->peerInfo.familyType,url,&urlData);
 
-	char* remoteSdp=(char*)yang_calloc(1,Yang_SDP_BUFFERLEN);
-
-	if(yang_http_post(yangtrue,peer->peerInfo.familyType,remoteSdp,urlData.server,
+	remoteSdp=(char*)yang_calloc(1,Yang_SDP_BUFFERLEN);
+	if(yang_https_post(yangtrue,peer->peerInfo.familyType,remoteSdp,urlData.server,
 			urlData.port, urlData.stream, (uint8_t*)localSdp, yang_strlen(localSdp))){
-		char* endp=yang_strstr(remoteSdp,"\r\n\r\n");
-		if(endp) yang_error("\n%s",endp+4);
-		yang_free(remoteSdp);
-		return yang_error_wrap(1,"query whip sdp failure!");
+		endp=yang_strstr(remoteSdp,"\r\n\r\n");
+
+		if(endp)
+			yang_error("\n%s",endp+4);
+
+		err=1;
+		yang_error("query whip sdp failure!");
+		goto cleanup;
 	}
 
 	if(yang_strstr(remoteSdp,"201")&&yang_strstr(remoteSdp,"Created")){
-		char* p2=yang_strstr(remoteSdp,"\r\n\r\n");
-		if(p2==NULL) return ERROR_RTC_Whip;
-		char* p=yang_strstr(p2,"v=0");
+		p2=yang_strstr(remoteSdp,"\r\n\r\n");
+		if(p2==NULL){
+			err=ERROR_RTC_Whip;
+			goto cleanup;
+		}
+
+
+		p=yang_strstr(p2,"v=0");
 		if(p){
-			int32_t len=yang_strlen(p);
-			char* sdp=(char*)yang_calloc(len+1,1);
+			len=yang_strlen(p);
+			sdp=(char*)yang_calloc(len+1,1);
 			yang_cstr_replace(p,sdp, "\r\n", "\n");
 			p=yang_strstr(sdp,"\n\n");
-			if(p) *p=0;
+			if(p)
+				*p=0;
+
 			*premoteSdp=sdp;
 			err=Yang_Ok;
 		}
 	}else{
 		err=ERROR_RTC_Whip;
 	}
+	cleanup:
 	yang_free(remoteSdp);
 	return err;
 
@@ -61,8 +75,6 @@ int32_t yang_whip_connectPeer(YangMetaConnection *conn,YangPeer* peer,char* url)
 
 	int err=Yang_Ok;
 	char* remoteSdp=NULL;
-
-
 
 	char *localSdp=NULL;
 	if((err=conn->createOffer(peer, &localSdp))!=Yang_Ok){
@@ -78,12 +90,12 @@ int32_t yang_whip_connectPeer(YangMetaConnection *conn,YangPeer* peer,char* url)
 
 	yang_free(localSdp);
 	yang_free(remoteSdp);
-    return err;
+	return err;
 }
 
 int32_t yang_whip_connectWhipWhepServer(YangPeer* peer,char* url){
 	YangMetaConnection conn={0};
-	yang_create_metaConnection(&conn);
+    yang_create_metaConnection(&conn);
 
 	if(peer==NULL)
 		return  ERROR_RTC_PEERCONNECTION;
@@ -96,9 +108,9 @@ int32_t yang_whip_connectWhipWhepServer(YangPeer* peer,char* url){
 
 }
 
-int32_t yang_whip_connectSfuServer(YangPeer* peer,char* url,int32_t mediaServer){
+int32_t yang_whip_connectSfuServer(YangPeer* peer,char* url,yangbool isHttps,int32_t mediaServer){
 	YangMetaConnection conn={0};
-	yang_create_metaConnection(&conn);
+    yang_create_metaConnection(&conn);
 
 	if(peer==NULL)
 		return  ERROR_RTC_PEERCONNECTION;
@@ -108,12 +120,10 @@ int32_t yang_whip_connectSfuServer(YangPeer* peer,char* url,int32_t mediaServer)
 
 
 	if(mediaServer==Yang_Server_Zlm)
-		return yang_zlm_connectRtcServer(&conn,peer,url);
+		return yang_zlm_connectRtcServer(&conn,peer,url,isHttps);
 	if(mediaServer==Yang_Server_Srs)
-		return yang_srs_connectRtcServer(&conn,peer,url);
+		return yang_srs_connectRtcServer(&conn,peer,url,isHttps);
 
 	return ERROR_RTC_PEERCONNECTION;
 }
-
-
 
